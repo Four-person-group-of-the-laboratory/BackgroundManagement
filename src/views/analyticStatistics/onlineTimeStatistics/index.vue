a4bcf680c0cba10fdc85115b3b1f4ba6b2d9c98d
likingcan <2806826282@qq.com>
2020-03-12 14:29:58 +0800

    无

--- src/views/analyticStatistics/onlineTimeStatistics/index.vue
+++ src/views/analyticStatistics/onlineTimeStatistics/index.vue
@@ -9,7 +9,7 @@
             <div>
               <img class="card" width="200px" src="https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191" />
             </div>
-            <div text-align:center>
+            <div>
               <router-link :to="{path:'',query:{}}"><el-button type="primary" >查看详情</el-button></router-link>
             </div>
           </el-card>
