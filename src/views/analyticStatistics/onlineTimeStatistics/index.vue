928c7df4d4a8c53e4aae7ddb066bb24d555f1ac0
likingcan <2806826282@qq.com>
2020-03-12 14:26:42 +0800

    无

--- src/views/analyticStatistics/onlineTimeStatistics/index.vue
+++ src/views/analyticStatistics/onlineTimeStatistics/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="chart-container">
-    <chart height="100%" width="100%" />
+  <div class="">
+    <div style="margin-top:50px;">
+      <el-col :span="4">
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>班级名称</span>
+            </div>
+            <div>
+              <img class="card" width="200px" src="https://wpimg.wallstcn.com/577965b9-bb9e-4e02-9f0c-095b41417191" />
+            </div>
+            <div text-align:center>
+              <router-link :to="{path:'',query:{}}"><el-button type="primary" >查看详情</el-button></router-link>
+            </div>
+          </el-card>
+        </el-col>
+      </div>
   </div>
 </template>
 
@@ -14,10 +28,13 @@ export default {
 </script>
 
 <style scoped>
-.chart-container{
-  position: relative;
-  width: 100%;
-  height: calc(100vh - 84px);
+.mixin-components-container {
+  background-color: #f0f2f5;
+  padding: 30px;
+  min-height: calc(100vh - 84px);
+}
+.component-item{
+  min-height: 100px;
 }
 </style>
 
